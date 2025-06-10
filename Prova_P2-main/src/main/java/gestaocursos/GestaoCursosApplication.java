cca22a4d31cb3ec39eeb7e2c5efdbee6541604f1
Maik Souza <119531950+MaikProgramador@users.noreply.github.com>
2025-06-10 19:13:47 +0000

    Alterações

--- Prova_P2-main/src/main/java/gestaocursos/GestaoCursosApplication.java
+++ Prova_P2-main/src/main/java/gestaocursos/GestaoCursosApplication.java
@@ -1,11 +1,12 @@
-package application;
+package com.example.gestaocursos; 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebApplication {
+public class GestaoCursosApplication { 
+
     public static void main(String[] args) {
-        SpringApplication.run(WebApplication.class, args);
+        SpringApplication.run(GestaoCursosApplication.class, args);
     }
 }
--- Prova_P2-main/src/main/java/gestaocursos/security/AuthenticationService.java
+++ Prova_P2-main/src/main/java/gestaocursos/security/AuthenticationService.java
@@ -8,18 +8,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-// Esta classe implementa a interface UserDetailsService do Spring Security.
-// A responsabilidade dela é única: carregar os dados de um usuário a partir de uma fonte de dados (nosso banco).
+
 public class AuthenticationService implements UserDetailsService {
 
     @Autowired
     private AlunoRepository repository;
 
-    // Este é o método que o Spring Security chama por baixo dos panos durante a autenticação.
-    // Ele usa o "username" (que no nosso caso é o email) para buscar o usuário.
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // Usamos o método que criamos no AlunoRepository para buscar pelo email.
+
         return repository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o email: " + username));
     }
